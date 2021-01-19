6d34e3f5425b70f51a98e3633c787e33c413532a
Arteom Balanuta <arteom.balanuta@gmail.com>
2021-01-19 03:03:30 +0200

    wip

--- jsonparser.c
+++ jsonparser.c
@@ -1,5 +1,51 @@
 #include "jsonparser.h"
 
+int
+count_double_quotes (unsigned char *string, int size)
+{
+  int quotesCount = 0;
+  for (int i = 0; i < size - 1; i++)
+    {
+      if (string[i] == '"')
+	{
+	  quotesCount++;
+	}
+    }
+  return quotesCount;
+}
+
+bool
+validate_json_key (unsigned char *key, int size)
+{
+  if (count_double_quotes (key, size) == 2 
+	&& key[0] == '"' && key[size - 1] == '"')
+	{
+	  return true;
+	}
+    }
+  return false;
+}
+
+bool
+validate_json_value (unsigned char *value, int size)
+{
+	if(count_double_quotes (value, size) == 2
+	  && value[0] == '"' && value[size-1] == '"')
+	{
+	return true;
+	}
+
+	if(count_double_quotes (value, size) > 2)
+	{
+	for(int i=1; i<size-2; i++){
+		if(value[i] == '"' && value[i-1] != '\'){
+		return false;
+		}
+	}
+	}	
+	return true;
+}
+
 void
 parse_hack_chat_json (unsigned char *json, unsigned char *nameMessageString)
 {
@@ -7,4 +53,8 @@ parse_hack_chat_json (unsigned char *json, unsigned char *nameMessageString)
   char[20] name = "";
   char[40] message = "";
 
-json}
+  
+
+
+
+}
